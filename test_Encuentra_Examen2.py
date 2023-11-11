fd626027dc7f26b096e5c2acb4f244b896e1018e
joscCE <j.campos@estudiantec.cr>
2023-11-11 14:19:11 -0600

    prueba unitaria para Encuentra

---
+++ test_Encuentra_Examen2.py
@@ -0,0 +1,7 @@
+from Encuentra_Examen2 import Encuentra
+import pytest
+
+def test_Encuentra():
+    assert Encuentra([1,2,3,4],4) == True
+    assert Encuentra([1, 2, 3, 5], 4) == False
+    assert Encuentra([1, 2, 3, 4,8], 2) == True
